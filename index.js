f16e20abb69dd2f4911c980251c00d797490c87a
Aya Ramirez <ayaramz@gmail.com>
2022-04-28 23:14:33 -0400

    commit tree 9912b9dc0bdf57e6c088930584e36ae72c903c6f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks === 42){
+        return 0
+    }else if(blocks === 43){
+        return 1
+    }else (blocks === 50)
+        return 8
+   }
+
+   
+function distanceFromHqInFeet(x){
+        return (x-42)*264
+}
+
+console.log(distanceFromHqInFeet(43));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceFromHqInFeet(34));
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+}
+
